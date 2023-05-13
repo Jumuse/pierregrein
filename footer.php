9621dc66a56e7dfac069d8c844988b4313b55377
Jumuse <justinehoguet@gmail.com>
2023-05-13 14:35:14 +0200

    updating index, header and footer

--- footer.php
+++ footer.php
@@ -2,13 +2,19 @@
 
 <div class="upwards">
     <img src="Images/Logo Light.png" class="logo-footer" alt="logo">
-    <p>Nous sommes le mardi 2 mai 2023
+    <p class="footerP">
+        <?php 
+            require_once 'dates.php';
+            echo "Aujourd'hui, ". dateToFrench("now" ,"l"). " ". dateToFrench("now" ,"j F Y");
+        ?>
         <br>
-        Le restaurant est ouvert de :
+        <?php
+            echo retrieveDate();
+        ?>
     </p>
 </div>
 
-<div class="middle">
+<div class="middlePart">
     <img src="Images/logo millaut.png" class="img-footer" alt="'Logo Millau">
     <img src="Images/logo millaut.png" class="img-footer" alt="'Logo Millau">
     <img src="Images/logo millaut.png" class="img-footer" alt="'Logo Millau">
@@ -16,54 +22,42 @@
 </div>
 
 <div class="downwards">
-    <p>mentions légales</p>
+    <p class="footerP">mentions légales</p>
 </div>
 
 </footer>
 
 <style>
-    body {
-        margin: 0;
-    }
+
     footer {
+        margin: 0;
+        display: block;
         background: #392C1E;
         color: #B6AC97;
-        font-size: 12px;
         font-family: system-ui, -apple-system, BlinkMacSystemFont, 'MontSerrat', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
-    .upwards, .middle, .downwards {
-        display: flex;
-        margin: 1%;
+
+    .footerP {
+        font-size: 13px;
     }
-    .upwards {
-        padding: 1em;
-        justify-content: space-around;
-        align-items: center;
-    } 
-    .upwards p {
+    .upwards, .downwards {
+        padding: 1px;
         text-align: center;
-    }
-    .middle {
-        justify-content: space-evenly;
-    }
-    .downwards {
-        justify-content: center;
-    }
+    } 
     .logo-footer {
          height: 30px;
     }
-    .img-footer {
-        height: 20px;
-    }
+
     @media screen and (min-width:640px){
         footer {
-            font-size:24px;
+            font-size:18px;
+            display: flex;
         }
         .logo-footer {
             height: 100px;
         }
         .img-footer {
-            height: 100px;
+            height: 70px;
         }
     }
 
---
+++ header.php
@@ -0,0 +1,61 @@
+<header>
+    <img src="Images/Logo Light.png" alt="Logo">
+
+    <nav role="navigation">
+        <div id="menuFullScreen">
+            <a href="#">Carte</a>
+            <a href="#">Menu</a>
+            <a href="#">Galerie</a>
+            <a href="#">Contact</a>
+            <a href="#">Réserver</a>
+            <a href="#">Se connecter</a>
+        </div>
+    </nav>
+
+</header>
+
+<style>
+
+    header {
+        background: #392C1E;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    img {
+        height: 30px;
+    }
+    #menuFullScreen {
+        display: none;
+    }
+
+    @media screen and (min-width:640px){
+        #menuFullScreen {
+            display: flex;
+        }
+    }
+    @media screen and (min-width:640px){
+        header {
+            padding: 5em;
+        }
+        img {
+            height: 120px;
+        }
+
+        a {
+            padding-left: 2em;
+            font-size: 25px;
+            text-decoration: none;
+            color: #B6AC97;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'MontSerrat', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+        a:hover {
+            color: #906427;
+        }
+        a:active {
+            color: #906427;
+            text-decoration: underline;
+        }
+    }
+</style>
